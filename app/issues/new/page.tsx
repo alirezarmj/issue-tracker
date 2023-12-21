75ce5901d2018ad4cf06c6c63f7e10f34a171c01
alirezarmj <a.rashidi.m78@gmail.com>
2023-12-21 21:38:36 +0330

    Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,46 +3,57 @@ import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import { useReducer } from "react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
   description: string;
 }
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
   return (
-    <form
-      className=" w-2/3 space-y-4"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <input
-        {...register("title")}
-        type="text"
-        placeholder="Title"
-        className=" w-full border px-2 py-1  "
-      />
-      {/* <textarea
+    <div className=" w-2/3">
+      {error && (
+        <p className="px-3 py-2 rounded-md mb-4 bg-rose-300">{error}</p>
+      )}
+      <form
+        className="  space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured");
+            console.log(error);
+          }
+        })}
+      >
+        <input
+          {...register("title")}
+          type="text"
+          placeholder="Title"
+          className=" w-full border px-2 py-1  "
+        />
+        {/* <textarea
         placeholder="Description"
         className=" w-full border px-2 py-1 "
       /> */}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <button className=" px-4 py-2  bg-cyan-700 rounded-md text-white ">
-        Submit New Issue
-      </button>
-    </form>
+        <button className=" px-4 py-2  bg-cyan-700 rounded-md text-white ">
+          Submit New Issue
+        </button>
+      </form>
+    </div>
   );
 };
 
