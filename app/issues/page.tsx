4409a0df84abaaea594d55e3a46d098eefc3402a
alirezarmj <a.rashidi.m78@gmail.com>
2023-12-22 07:53:44 +0330

    Show the Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,55 @@
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+  console.log(issues);
   return (
-    <div>
-      <button className=" px-4 py-2  bg-cyan-700 rounded-md text-white">
-        <Link href="/issues/new">new issue</Link>
-      </button>
+    // <div>
+    //   <button className=" px-4  py-2  bg-cyan-700 rounded-md text-white">
+    //     <Link href="/issues/new">new issue</Link>
+    //   </button>
+    // </div>
+    <div className="container mx-auto">
+      <div className="my-4">
+        <button className="px-4  py-2 bg-cyan-700 rounded-md text-white">
+          <Link href="/issues/new">New Issue</Link>
+        </button>
+      </div>
+      <div className="overflow-x-auto rounded-md">
+        <table className="min-w-full border ">
+          <thead>
+            <tr>
+              <th className="px-6 py-3  bg-purple-50 text-left  text-base leading-4   font-semibold text-gray-500 uppercase tracking-wider">
+                Issue
+              </th>
+              <th className="px-6 py-3 hidden md:table-cell   bg-purple-50 text-left  text-base leading-4   font-semibold text-gray-500 uppercase tracking-wider">
+                Status
+              </th>
+              <th className="px-6 py-3 hidden md:table-cell   bg-purple-50 text-left  text-base leading-4   font-semibold text-gray-500 uppercase tracking-wider">
+                Created
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {issues.map((issue) => (
+              <tr key={issue.id} className="border-t border-gray-200">
+                <td className="px-6 py-4  whitespace-no-wrap">
+                  {issue.title}
+                  <div className="block  md:hidden">{issue.status}</div>
+                </td>
+
+                <td className="px-6 py-4 hidden md:table-cell whitespace-no-wrap">
+                  {issue.status}
+                </td>
+                <td className="px-6 py-4 hidden md:table-cell whitespace-no-wrap">
+                  {issue.createdAt.toDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
