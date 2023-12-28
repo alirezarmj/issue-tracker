beb1b7acc8db1d81e09c6fe0d19c30ee8f6d6bde
alirezarmj <a.rashidi.m78@gmail.com>
2023-12-28 11:00:50 +0330

    show a confirmation dialog box

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,14 +2,26 @@ import React, { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   color?: string; // Adding color as a prop
+  width?: string; // Adding width as a prop
 };
 
-const Button: React.FC<ButtonProps> = ({ children, color, ...props }) => {
-  const buttonColor = color === "red" ? `bg-red-700` : "bg-cyan-700"; // Setting the button color based on the prop or default
+const Button: React.FC<ButtonProps> = ({
+  children,
+  color,
+  width,
+  ...props
+}) => {
+  const buttonColor =
+    color === "red"
+      ? `bg-red-700`
+      : color === "gray"
+      ? "bg-gray-700"
+      : "bg-cyan-700"; // Setting the button color based on the prop or default
+  const buttonWidth = width || "auto"; // Using the provided width or defaulting to 'auto'
 
   return (
     <button
-      className={`px-4 py-2 flex  w-full justify-center whitespace-nowrap items-center space-x-2 rounded-md text-white ${buttonColor}`}
+      className={`px-4 py-2 flex w-${buttonWidth} justify-center whitespace-nowrap items-center space-x-2 rounded-md text-white ${buttonColor}`}
       {...props}
     >
       {children}
@@ -18,3 +30,24 @@ const Button: React.FC<ButtonProps> = ({ children, color, ...props }) => {
 };
 
 export default Button;
+
+// import React, { ButtonHTMLAttributes } from "react";
+
+// type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+//   color?: string; // Adding color as a prop
+// };
+
+// const Button: React.FC<ButtonProps> = ({ children, color, ...props }) => {
+//   const buttonColor = color === "red" ? `bg-red-700` : "bg-cyan-700"; // Setting the button color based on the prop or default
+
+//   return (
+//     <button
+//       className={`px-4 py-2 flex  w-full justify-center whitespace-nowrap items-center space-x-2 rounded-md text-white ${buttonColor}`}
+//       {...props}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+// export default Button;
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,80 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
 import Button from "@/app/components/Button";
-import React from "react";
 
-const DeleteIssueButton = ({ issudeId }: { issudeId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null); // Ensure proper typing for modalRef
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleDelete = () => {
+    // Perform your delete action here
+    console.log(`Deleting issue ${issueId}`);
+    closeModal(); // Close the modal after deletion
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    if (modalIsOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [modalIsOpen]);
+
+  return (
+    <>
+      <Button color="red" width="full" onClick={openModal}>
+        Delete Issue
+      </Button>
+      {modalIsOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="fixed inset-0 bg-black opacity-50"></div>
+            <div className="bg-white rounded-lg p-8 z-20" ref={modalRef}>
+              <h2 className="text-2xl mb-4">Confirm Deletion</h2>
+              <p className="mb-4">
+                Are you sure you want to delete this issue? This action cannot
+                be undone.
+              </p>
+              <div className="flex justify-end">
+                <Button onClick={closeModal} color="gray">
+                  Cancel
+                </Button>
+                <Button onClick={handleDelete} color="red" className="mr-2">
+                  Delete Issue
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default DeleteIssueButton;
+
+// import Button from "@/app/components/Button";
+
+// const DeleteIssueButton = ({ issudeId }: { issudeId: number }) => {
+//   return <Button color="red">Delete Issue</Button>;
+// };
+
+// export default DeleteIssueButton;
