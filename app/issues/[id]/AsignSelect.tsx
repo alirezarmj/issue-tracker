7cf703c98709248deecbc8bf6104fe5923fcf659
alirezarmj <a.rashidi.m78@gmail.com>
2023-12-29 01:15:22 +0330

    fetch data with react query

--- app/issues/[id]/AsignSelect.tsx
+++ app/issues/[id]/AsignSelect.tsx
@@ -1,18 +1,34 @@
 "use client";
 
 import { User } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
+// import Skeleton from "react-loading-skeleton";
+import { Skeleton } from "@/app/components";
 
 const AsignSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+  if (isLoading) return <Skeleton height="2.5rem" />;
+  if (error) return null;
+
+  //   const [users, setUsers] = useState<User[]>([]);
+  //   useEffect(() => {
+  //     const fetchUsers = async () => {
+  //       const { data } = await axios.get<User[]>("/api/users");
+  //       setUsers(data);
+  //     };
+  //     fetchUsers();
+  //   }, []);
   return (
     <div>
       {/* <label htmlFor="suggestions">Suggestions</label> */}
@@ -23,7 +39,7 @@ const AsignSelect = () => {
         <option value="" defaultValue="Assign">
           Assign
         </option>
-        {users.map((user) => (
+        {users?.map((user) => (
           <option key={user.id} value={user.id}>
             {user.name}
           </option>
