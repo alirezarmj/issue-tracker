c12241f17c93d228dd38bd3398a3b6c1760054a9
alirezarmj <a.rashidi.m78@gmail.com>
2023-12-30 20:35:42 +0330

    Implement Pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import Button from "./Button";
 import {
   BsChevronDoubleLeft,
@@ -14,22 +16,48 @@ interface Props {
 
 const Pagination = ({ currentPage, itemCount, pageSize }: Props) => {
   const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  function changePage(page: number) {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  }
+
   if (pageCount <= 1) return null;
+
   return (
     <div className=" flex items-center space-x-2">
       <p>
         page {currentPage} of {pageCount}
       </p>
-      <Button color="gray" disabled={currentPage === 1}>
+      <Button
+        onClick={() => changePage(1)}
+        color="gray"
+        disabled={currentPage === 1}
+      >
         <BsChevronDoubleLeft />
       </Button>
-      <Button color="gray" disabled={currentPage === 1}>
+      <Button
+        onClick={() => changePage(currentPage - 1)}
+        color="gray"
+        disabled={currentPage === 1}
+      >
         <BsChevronLeft />
       </Button>
-      <Button color="gray" disabled={currentPage === pageCount}>
+      <Button
+        onClick={() => changePage(currentPage + 1)}
+        color="gray"
+        disabled={currentPage === pageCount}
+      >
         <BsChevronRight />
       </Button>
-      <Button color="gray" disabled={currentPage === pageCount}>
+      <Button
+        onClick={() => changePage(pageCount)}
+        color="gray"
+        disabled={currentPage === pageCount}
+      >
         <BsChevronDoubleRight />
       </Button>
     </div>
