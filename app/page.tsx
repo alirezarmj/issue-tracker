497d5f9ae5aec309f5b29a800a98727d663a54af
alirezarmj <a.rashidi.m78@gmail.com>
2024-01-02 00:23:36 +0330

    Lay out the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -9,5 +9,13 @@ export default async function Home() {
     where: { status: "IN_PROGRESS" },
   })!;
   const closed = await prisma?.issue.count({ where: { status: "CLOSED" } })!;
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <div className=" grid md:grid-cols-2 grid-cols-1 gap-4">
+      <div className=" flex flex-col gap-4">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </div>
+      <LatestIssues />
+    </div>
+  );
 }
